d99e412ec2c59e6118627195f7eed112145eda66
masumjn <masumjuniyanto06@gmail.com>
2024-04-03 01:16:55 +0700

    add new file

--- app-old.js
+++ app-old.js
@@ -3,21 +3,44 @@ const app = express();
 const server = require("http").Server(app);
 const url = require("url");
 const cors = require("cors");
-const WebSocket = require("ws");
+var proxy = require("html2canvas-proxy");
 
-// Enable CORS for specific routes
-app.use("/jpgstream_server", cors());
-app.use("/jpgstream_client", cors());
+// app.use(
+//   cors({
+//     // origin: ['*', 'https://mownylive.online/','http://localhost/OptimizeDetect'],
+//     origin: '*',
+//     optionsSuccessStatus: 200,
+//   })
+// );
+
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
+app.use(
+  "/",
+  cors({ origin: "*" }),
+  // function (req, res, next) {}
+);
+
+// app.use('/', proxy());
+
+const WebSocket = require("ws");
 
 const port = process.env.PORT || 3000;
 
-// Assuming express_config.init(app) sets up some configurations for your Express app
+const express_config = require("./config/express.js");
 
-// Initialize WebSocket servers
+express_config.init(app);
+
 const wss1 = new WebSocket.Server({ noServer: true });
 const wss2 = new WebSocket.Server({ noServer: true });
 
-// Handle WebSocket connections
+var cameraArray = {};
+
+//esp32cam websocket
 wss1.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
     wss2.clients.forEach(function each(client) {
@@ -28,13 +51,14 @@ wss1.on("connection", function connection(ws) {
   });
 });
 
+//webbrowser websocket
 wss2.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
+    // nothing here should be received
     console.log("received wss2: %s", message);
   });
 });
 
-// Handle WebSocket upgrade requests
 server.on("upgrade", function upgrade(request, socket, head) {
   const pathname = url.parse(request.url).pathname;
 
@@ -51,12 +75,10 @@ server.on("upgrade", function upgrade(request, socket, head) {
   }
 });
 
-// Define a route to serve index.html or any other view
 app.get("/", (req, res) => {
   res.render("index", {});
 });
 
-// Start the server
 server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
